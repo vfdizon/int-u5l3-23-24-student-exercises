44a0ee3bcf38fa3a9eecab1eeca01212a8d7e00a
CN-Mika <83977863+CN-Mika@users.noreply.github.com>
2024-02-06 19:41:22 -0500

    Add coding exercise

---
+++ forEachLoops.js
@@ -0,0 +1,27 @@
+let array = [1, 2, 3, 4, 5];
+// 1. Use a forEach loop to print each element to the console.
+
+
+
+
+// 2. Create an array of colors ['red', 'green', 'blue']. 
+//  - Use a forEach loop to add "Color:" before each element and print them to the console. 
+//  - The output should look like "Color: red", "Color: green", "Color: blue".
+
+
+
+let numbers = [1, 2, 3, 4, 5];
+let sum = 0;
+// 3. Use a forEach loop to calculate and print the sum of all the elements in the array.
+
+
+
+let fruits = ["apple", "banana", "cherry"]
+// 4. Use a forEach loop to print each element to the console.
+
+
+
+let names = ["Simba", "Scar", "Nala", "Mufasa", "Zazu"];
+// 5. Use a forEach loop to print each name along with its index number.
+//  - Remember: the index can be put as a second parameter.
+//  - Example: the first message should read something like "Simba (Index: 0)"
