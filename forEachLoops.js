1cb02647720315acb3c3eef61106e2d630dd9de3
Vincent Dizon <vdizon@cps.edu>
2024-04-08 15:32:19 +0000

    finished tasks

--- forEachLoops.js
+++ forEachLoops.js
@@ -1,5 +1,8 @@
 let array = [1, 2, 3, 4, 5];
 // 1. Use a forEach loop to print each element to the console.
+array.forEach(function(val){
+    console.log(val);
+});
 
 
 
@@ -7,18 +10,27 @@ let array = [1, 2, 3, 4, 5];
 // 2. Create an array of colors ['red', 'green', 'blue']. 
 //  - Use a forEach loop to add "Color:" before each element and print them to the console. 
 //  - The output should look like "Color: red", "Color: green", "Color: blue".
-
+let colors = ['red', 'green', 'blue'];
+colors.forEach(function(val){
+    console.log("Color: " + val);
+});
 
 
 let numbers = [1, 2, 3, 4, 5];
 let sum = 0;
 // 3. Use a forEach loop to calculate and print the sum of all the elements in the array.
 //  - There's a couple ways to solve this one - think through the logic and ask for support if you need it :)
-
+numbers.forEach(function(val) {
+    sum += val;
+});
+console.log("sum: " + sum);
 
 
 let fruits = ["apple", "banana", "cherry"]
 // 4. Use a forEach loop to print each element to the console.
+fruits.forEach(function(val){
+    console.log(val);
+});
 
 
 
@@ -26,3 +38,6 @@ let names = ["Simba", "Scar", "Nala", "Mufasa", "Zazu"];
 // 5. Use a forEach loop to print each name along with its index number.
 //  - Remember: the index can be put as a second parameter.
 //  - Example: the first message should read something like "Simba (Index: 0)"
+names.forEach(function(val, idx){
+    console.log(val + "(Index: " + idx + ")");
+});
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let body = document.querySelector("body");
 
 // Array of image links
-let photos = ["https://preview.redd.it/if-the-show-is-successful-what-would-be-good-seasonal-pacing-v0-pwepl905kdlb1.jpg?width=640&crop=smart&auto=webp&s=1ccf2f03a4a9cee216bfdf27fdfac95d28392fd9",
-    "https://i1.wp.com/www.oxfordstudent.com/wp-content/uploads/2016/11/black-mirror-season-3-poster.png?fit=1080%2C1080&ssl=1",
-    "https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Grey%27s_Anatomy_Logo.svg/480px-Grey%27s_Anatomy_Logo.svg.png",
-    "https://assets.fxnetworks.com/cms-next/production/67a0aace-e484-4032-885b-c6f6a405c7eb.jpg"
+let photos = ["https://www.paintingmania.com/arts/francisco-de-goya/large/saturn-devouring-his-son-227_38960.jpg?version=19.11.19",
+    "https://www.paintingmania.com/arts/francisco-de-goya/large/saturn-devouring-his-son-227_38960.jpg?version=19.11.19",
+    "https://www.paintingmania.com/arts/francisco-de-goya/large/saturn-devouring-his-son-227_38960.jpg?version=19.11.19",
+    "https://www.paintingmania.com/arts/francisco-de-goya/large/saturn-devouring-his-son-227_38960.jpg?version=19.11.19",
+    "https://www.paintingmania.com/arts/francisco-de-goya/large/saturn-devouring-his-son-227_38960.jpg?version=19.11.19",
+    "https://www.paintingmania.com/arts/francisco-de-goya/large/saturn-devouring-his-son-227_38960.jpg?version=19.11.19",
 ];
 
 
 // 1. Push an image link of one of your favorite shows to the photos array.
 //  - To find the link, right-click an image and choose 'Copy Image Address'
+
 
 
 
@@ -22,6 +24,15 @@ let show4 = document.querySelector(".show-4");
 let show5 = document.querySelector(".show-5");
 let show6 = document.querySelector(".show-6");
 
+let divs = [];
+
 
 show1.src = photos[0];
 // 2. Update the src for each show# variable - use the line above as a reference.
+for(let i = 0; i < photos.length; i++) {
+    divs.push(document.querySelector(".show-" + (i+1)));
+}
+for(let i = 0 ; i < divs.length; i++) {
+   console.log(photos[i]);
+   divs[i].src = photos[i];
+}
